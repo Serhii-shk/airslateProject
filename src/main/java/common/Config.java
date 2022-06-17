f00660175da6021e4c087a33f548f00ae9f55524
Serhii <s.shkurenko@treeum.net>
2022-06-17 16:46:03 +0300

    Change default browser on Chrome

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -4,7 +4,7 @@ import com.codeborne.selenide.Configuration;
 
 public class Config {
     /** Specify the browser to test:*/
-    public static String BROWSER_NAME = "firefox"; //chrome firefox opera edge ie
+    public static String BROWSER_NAME = "chrome"; //chrome firefox opera edge ie
     /**Whether to clean the browser's cookies after each iteration*/
     public static boolean CLEAR_COOKIES = true;
     /**Clean the directory with the screen before starting the build*/
