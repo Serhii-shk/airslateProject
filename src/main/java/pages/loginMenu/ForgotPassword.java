b3517fc74f57db375eba946ec73d7e634855e3f3
Serhii <s.shkurenko@treeum.net>
2022-07-03 14:13:28 +0300

    fix "Forgot password" test; detection of a bug - if the login contains spaces, then the authorization fails (Only for FireFox)

--- src/main/java/pages/loginMenu/ForgotPassword.java
+++ src/main/java/pages/loginMenu/ForgotPassword.java
@@ -4,17 +4,21 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import pages.base.BasePage;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class ForgotPassword extends BasePage {
 
-    private final SelenideElement forgotPasswordButton = $x("//button[@aria-label='Forgot password?']");
+    private final SelenideElement forgotPasswordButton = $x("//button[@class='sl-button sl-button--width-auto sl-button--link']");
     private final SelenideElement recoveryEmailField = $x("//input[@data-qa-tag='email-input']");
     private final SelenideElement recoveryMyPassButton = $x("//button[@data-qa-tag='Recover My Password']");
 
     /** Clicking a Forgot password button */
     public ForgotPassword clickForgotPasswordButton() {
-        forgotPasswordButton.shouldBe(Condition.visible).click();
+//        forgotPasswordButton.shouldBe(Condition.visible).click();
+        forgotPasswordButton.shouldBe(visible, Duration.ofSeconds(5)).click();
         return this;
     }
 
@@ -29,7 +33,7 @@ public class ForgotPassword extends BasePage {
 
     /** Clicking a recovery my password button */
     public ForgotPassword clickRecoveryMyPassButton() {
-        recoveryMyPassButton.shouldBe(Condition.visible).click();
+        recoveryMyPassButton.shouldBe(visible).click();
         return this;
     }
 
